1a73656481b156c64ce6e0abe9f619fbe3bff979
Sadiya Parayil <sadiyahu.chatham@login1.ufhpc>
2022-09-21 00:17:48 -0400

    adding varaibles

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Program to output a system information page
+title ="system information report"
+
 echo "<html>
  <head>
-      <title>system information report</title>
+      <title>$title</title>
     </head>
-      <h1>system report</h1>
+      <h1>$title</h1>
  <body>
 Page body
 </body>
